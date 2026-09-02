73dfffdec0e22fcde4db0fc92db5c4a09f03c078
HwanE <jnl9327@naver.com>
2026-09-02 22:46:09 +0900

    first commit

---
+++ src/main/java/com/example/HwanE_gateway/HwanEGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.example.HwanE_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwanEGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HwanEGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/HwanE_gateway/HwanEGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HwanE_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HwanEGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
